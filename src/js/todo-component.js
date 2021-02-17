7c313d6039d25e91e51b434512418119405b6d64
Volodymyr Buts <wowabuz@gmail.com>
2021-02-18 00:01:28 +0200

    Init commit

---
+++ src/js/todo-component.js
@@ -0,0 +1,10 @@
+class ToDo {
+  constructor(title, status, description, id) {
+    this.id = id;
+    this.title = title;
+    this.status = status;
+    this.description = description;
+  }
+}
+
+export default ToDo;
