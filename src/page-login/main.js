a64e571ccb2677425d0887d2a320e122f88510a7
viktor.shslyapin <v.shalyapin3323@gmail.com>
2021-02-23 11:14:01 +0200

    first steps

--- src/page-login/main.js
+++ src/page-login/main.js
@@ -2,3 +2,13 @@ import 'normalize.css/normalize.css';
 
 import '../css/dashboard.css';
 import '../css/login.css';
+
+const card = new ToDo('title', 'status', 'description', 11);
+
+window.document.addEventListener('DOMContentLoaded', () => {
+  window.document.body.innerHTML = `<p>${Object.entries(card)} <p/>
+  <p>Registration</p>
+  <a href='./index.html'>Index</a>
+  <a href='./login.html'>Login</a>
+  `;
+});
