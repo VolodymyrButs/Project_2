159cf808c5abed8fa1eacb29057a3cbc962d6748
Volodymyr Buts <wowabuz@gmail.com>
2021-02-23 09:57:48 +0200

    Add reg ability

--- src/page-login/main.js
+++ src/page-login/main.js
@@ -1,4 +1,9 @@
 import 'normalize.css/normalize.css';
-
 import '../css/dashboard.css';
 import '../css/login.css';
+
+import pic from '../img/StartPic.png';
+
+window.document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('start-pic').setAttribute('src', pic);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset',
+      },
     ],
   },
 
